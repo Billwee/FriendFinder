12f594be6d4bff67aa922280a6f8d30badd90957
Bill Kamberos <bill.kamberos@gmail.com>
2020-03-22 20:36:28 -0400

    Routing finished - Form Validation Updated

---
+++ app/data/friends.js
@@ -0,0 +1,16 @@
+let friendsArr = [
+  {
+    name: 'Ahmed',
+    photo:
+      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    name: 'Not Ahmed',
+    photo:
+      'https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg',
+    scores: [1, 2, 3, 3, 4, 4, 6, 3, 2, 4]
+  }
+];
+
+module.exports = friendsArr;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,25 @@
 var path = require('path');
 
+var array = require('../data/friends');
+
 module.exports = function(app) {
   app.get('/api/friends', function(req, res) {
-    return res.json(friends);
+    return res.json(array);
   });
 
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'));
+  app.post('/api/friends', function(req, res) {
+    // array.push(req.body);
+    // console.log(req.body);
+    // console.log(array);
+    // res.json('sdfsdfsd');
+
+    array.forEach(element1 => {
+      for (let i = 0; i < req.body.scores.length; i++) {
+        let diff;
+        diff = req.body.scores[i] - element1.scores[i];
+        diff = Math.abs(diff);
+        // console.log(diff);
+      }
+    });
   });
 };
